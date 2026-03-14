95a51f67f782754ca293013ba57c643e6659ba27
coosti <costanzama21@gmail.com>
2026-03-14 19:44:06 +0100

    implement random honeypot placement strategy

--- src/honeypot.py
+++ src/honeypot.py
@@ -1,12 +1,17 @@
+import random
+
 from digital_twin import *
 from lateral_movement import *
 
 # --- HONEYPOT CLASS ---
 
 class Honeypot:
-    def __init__(self, dt):
+    def __init__(self, dt, num_honeypots = 5):
         self.dt = dt
         self.graph = dt.graph
+        self.subnets_map = dt.get_subnets()
+
+        self.num_honeypots = num_honeypots
 
         self.initialize_honeypots()
 
@@ -18,3 +23,26 @@ class Honeypot:
         for n, data in self.graph.nodes(data = True):
             if data.get('type') in types:
                 self.graph.nodes[n]['is_honeypot'] = False
+
+    # pick n random nodes between assets
+    def random_strategy(self):
+
+        self.initialize_honeypots()
+
+        assets = []
+
+        for hosts in self.subnets_map.values():
+            for asset in hosts:
+                assets.append(asset)
+
+        # include routers
+        for router, data in self.graph.nodes(data = True):
+            if data.get('type') == 'Router' and router != 'Router_0':
+                assets.append(router)
+
+        chosen_assets = random.sample(assets, self.num_honeypots)
+
+        for asset in chosen_assets:
+            self.graph.nodes[asset]['is_honeypot'] = True
+
+        return chosen_assets
