4a7c85fdf98909e2a68966b29089425ef0c04786
coosti <costanzama21@gmail.com>
2026-03-16 19:15:05 +0100

    implement max cvss honeypot placement strategy

--- src/honeypot.py
+++ src/honeypot.py
@@ -13,7 +13,21 @@ class Honeypot:
 
         self.num_honeypots = num_honeypots
 
+        self.assets = self.initialize_assets()
+
         self.initialize_honeypots()
+
+    def initialize_assets(self):
+        assets = []
+
+        for hosts in self.subnets_map.values():
+            for asset in hosts:
+                assets.append(asset)
+
+        # include routers
+        for router, data in self.graph.nodes(data = True):
+            if data.get('type') == 'Router' and router != 'Router_0':
+                assets.append(router)
 
     def initialize_honeypots(self):
 
@@ -29,18 +43,37 @@ class Honeypot:
 
         self.initialize_honeypots()
 
-        assets = []
+        chosen_assets = random.sample(self.assets, self.num_honeypots)
 
-        for hosts in self.subnets_map.values():
-            for asset in hosts:
-                assets.append(asset)
+        for asset in chosen_assets:
+            self.graph.nodes[asset]['is_honeypot'] = True
 
-        # include routers
-        for router, data in self.graph.nodes(data = True):
-            if data.get('type') == 'Router' and router != 'Router_0':
-                assets.append(router)
+        return chosen_assets
+    
+    # pick n assets with max score
+    def max_cvss_strategy(self):
+        
+        self.initialize_honeypots()
 
-        chosen_assets = random.sample(assets, self.num_honeypots)
+        sorted_assets = []
+
+        for n in self.assets:
+
+            score = self.graph.nodes[n].get('asset_score')
+
+            if score > 0:
+
+                attack_surface = sum(1 for sw in self.graph.successors(n)
+                                        if self.graph.nodes[sw].get('type') == 'Software' and self.graph.nodes[sw].get('max_cvss', 0.0) > 4.0)
+                
+                sorted_assets.append((n, score, attack_surface))
+
+        # check empty list case 
+        
+        # in case of asset score equality order by attack surface
+        sorted_assets.sort(key=lambda x: (x[1], x[2]), reverse=True)
+
+        chosen_assets = [x[0] for x in sorted_assets[:self.num_honeypots]]
 
         for asset in chosen_assets:
             self.graph.nodes[asset]['is_honeypot'] = True
